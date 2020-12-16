5060c9a08c500281378a43428466e5adfe5ecc53
samuel.son <samuel.son1007@gmail.com>
2020-12-16 23:46:42 +0900

    adding npm test taskrun

--- node_sample/test/example.test.js
+++ node_sample/test/example.test.js
@@ -5,12 +5,29 @@ var EthUtil = require('ethereumjs-util');
 
 describe('Simple Math Test', () => {
     it('should return 2', () => {
+        assert.strictEqual(1 + 1, 2);
+    });
+    it('should return 9', () => {
+        assert.strictEqual(3 * 3, 9);
+    });
+});
+
+describe('Simple Math Test FAIL', () => {
+    it('should return 2', () => {
+        assert.strictEqual(1, 2);
+    });
+});
+
+
+describe('Simple Math Test', () => {
+    it('should return 2', () => {
         assert.equal(1 + 1, 2);
     });
     it('should return 9', () => {
         assert.equal(3 * 3, 9);
     });
 });
+
 
 describe('Ethereum Test', () => {
 
